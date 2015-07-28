7a30471e600d3abf9461e1e3eef26ab464d56dbe
Itamar Haber <itamar@redislabs.com>
2015-07-28 13:42:01 +0300

    run.sh readded

---
+++ run.sh
@@ -0,0 +1,1 @@
+nohup time java -server -Xms2g -Xmx2g -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:CMSInitiatingOccupancyFraction=60 -XX:+UseCMSInitiatingOccupancyOnly -XX:+ExplicitGCInvokesConcurrent -jar lsu-1.0-SNAPSHOT-jar-with-dependencies.jar 192.168.70.101 13409 /run/shm/metagenome 55821043 100 true 1 &
